6bfacae8461d8ca6138dfbcd4c9c7c6eaa43e439
William Law <will@willlaw.org>
2023-08-16 10:03:53 -0400

    Various small fixes and updates:
    * Fix right hand home row CTRL
    * Remove left thumb LGUI
    * Add homerow mods to LOWER and RAISE layers (except for left LOWER layer because of conflicts with ^&( and mod tap)
    * Remove previous and next tab from LOWER
    * Move max win from ‘ to / key

--- keyboards/lily58/keymaps/williumbillium_hrm/keymap.c
+++ keyboards/lily58/keymaps/williumbillium_hrm/keymap.c
@@ -47,16 +47,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \BackSp\  |Space |Delete|      |
- *                   |      | LGUI |RAISE |/       /         \      \ |LOWER |      |      |
+ *                   |      |      |RAISE |/       /         \      \ |LOWER |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX,     XXXXXXX,       XXXXXXX,                             XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,        XXXXXXX,
-  XXXXXXX, KC_Q,         KC_W,        KC_E,        KC_R,          KC_T,                                KC_Y,              KC_U,        KC_I,        KC_O,        KC_P,           XXXXXXX,
-  XXXXXXX, CTL_T(KC_A),  ALT_T(KC_S), GUI_T(KC_D), SFT_T(KC_F),   KC_G,                                KC_H,              SFT_T(KC_J), GUI_T(KC_K), ALT_T(KC_L), SFT_T(KC_QUOT), XXXXXXX,
-  XXXXXXX, KC_Z,         KC_X,        KC_C,        KC_V,          KC_B,              XXXXXXX, XXXXXXX, KC_N,              KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        XXXXXXX,
-                                      XXXXXXX,     GUI_T(KC_ESC), LT(_RAISE,KC_TAB), KC_ENT,  KC_BSPC, LT(_LOWER,KC_SPC), KC_DEL,      XXXXXXX
+  XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,                             XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,        XXXXXXX,
+  XXXXXXX, KC_Q,         KC_W,        KC_E,        KC_R,        KC_T,                                KC_Y,              KC_U,        KC_I,        KC_O,        KC_P,           XXXXXXX,
+  XXXXXXX, CTL_T(KC_A),  ALT_T(KC_S), GUI_T(KC_D), SFT_T(KC_F), KC_G,                                KC_H,              SFT_T(KC_J), GUI_T(KC_K), ALT_T(KC_L), CTL_T(KC_QUOT), XXXXXXX,
+  XXXXXXX, KC_Z,         KC_X,        KC_C,        KC_V,        KC_B,              XXXXXXX, XXXXXXX, KC_N,              KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        XXXXXXX,
+                                      XXXXXXX,     KC_ESC,      LT(_RAISE,KC_TAB), KC_ENT,  KC_BSPC, LT(_LOWER,KC_SPC), KC_DEL,      XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -64,20 +64,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      | Back | Fwd  |VolUp |                    |CMD+Lt|CMD+Dn|CMD+Up|CMD+Rt|Scrnst|      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      |PrvTab|NxtTab|VolDn |-------.    ,-------| Left | Down |  Up  |Right |MaxWin|      |
+ * |      |      |      |      |      |VolDn |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |ShwDsk|PrvDsk|NxtDsk| Mute |-------|    |-------|ALT+Lt| PgDn | PgUp |ALT+Rt|      |      |
+ * |      |      |ShwDsk|PrvDsk|NxtDsk| Mute |-------|    |-------|ALT+Lt| PgDn | PgUp |ALT+Rt|MaxWin|      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \BackSp\  |Space |Delete|      |
  *                   |      | LGUI |RAISE |/       /         \      \ |LOWER |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______,      _______,     _______,                   _______,    _______,    _______,  _______,    _______,    _______,
-  _______, _______, _______, KC_BTN4,      KC_BTN5,     KC_VOLU,                   G(KC_LEFT), G(KC_DOWN), G(KC_UP), G(KC_RGHT), G(S(KC_4)), _______,
-  _______, _______, _______, C(S(KC_TAB)), C(KC_TAB),   KC_VOLD,                   KC_LEFT,    KC_DOWN,    KC_UP,    KC_RGHT,    C(G(KC_Z)), _______,
-  _______, _______, KC_F11,  C(KC_LEFT),   C(KC_RIGHT), KC_MUTE, _______, _______, A(KC_LEFT), KC_PGDN,    KC_PGUP,  A(KC_RGHT), _______,    _______,
-                             _______,      _______,     _______, _______, _______,  _______,   _______,    _______
+  _______, _______, _______, _______,    _______,     _______,                   _______,    _______,        _______,      _______,        _______,    _______,
+  _______, _______, _______, KC_BTN4,    KC_BTN5,     KC_VOLU,                   G(KC_LEFT), G(KC_DOWN),     G(KC_UP),     G(KC_RGHT),     G(S(KC_4)), _______,
+  _______, KC_LCTL, KC_LALT, KC_LGUI,    KC_LSFT,     KC_VOLD,                   KC_LEFT,    SFT_T(KC_DOWN), GUI_T(KC_UP), ALT_T(KC_RGHT), KC_LCTL,    _______,
+  _______, _______, KC_F11,  C(KC_LEFT), C(KC_RIGHT), KC_MUTE, _______, _______, A(KC_LEFT), KC_PGDN,        KC_PGUP,      A(KC_RGHT),     C(G(KC_Z)), _______,
+                             _______,    _______,     _______, _______, _______,  _______,   _______,        _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -95,11 +95,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [_RAISE] = LAYOUT(
-  TG(_ADJUST), _______, _______, _______, _______, _______,                     _______, _______, _______, _______, _______, _______,
-  _______,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_PLUS, KC_7,    KC_8,    KC_9,    KC_EQL,  _______,
-  _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                     KC_MINS, KC_4,    KC_5,    KC_6,    KC_SCLN, _______,
-  _______,     KC_TILD, KC_GRV,  KC_UNDS, KC_LBRC, KC_RBRC,  _______, _______,  KC_0,    KC_1,    KC_2,    KC_3,    KC_BSLS, _______,
-                                 _______, _______, _______,  _______, _______,  _______, _______, _______
+  TG(_ADJUST), _______, _______, _______, _______, _______,                     _______, _______,     _______,     _______,     _______,        _______,
+  _______,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_PLUS, KC_7,        KC_8,        KC_9,        KC_EQL,         _______,
+  _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                     KC_MINS, SFT_T(KC_4), GUI_T(KC_5), ALT_T(KC_6), CTL_T(KC_SCLN), _______,
+  _______,     KC_TILD, KC_GRV,  KC_UNDS, KC_LBRC, KC_RBRC,  _______, _______,  KC_0,    KC_1,        KC_2,        KC_3,        KC_BSLS,        _______,
+                                 _______, _______, _______,  _______, _______,  _______, _______,     _______
 ),
 
 /* ADJUST
