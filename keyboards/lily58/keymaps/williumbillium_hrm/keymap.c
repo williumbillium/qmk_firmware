c9468f4f7048abc76bec3a8764ce13f7af09ec44
William Law <will@willlaw.org>
2023-11-21 10:18:54 -0500

    Change from MacOS to Windows layout: * Switch CMD and CTRL (in all layers) * ALT-left/right to CTRL-left/rigt * CMD-up/down to CTRL-home/end * CMD-left/right to home/end * Add layer tap on left thumb for layer 3 * Add F keys to right side of layer 3 * Change left/right desktop shortcut to Windows * Remove F11/show desktop shortcut * Change name of layer 3 to FUNC * Remove existing Adjust layer toggle

--- keyboards/lily58/keymaps/williumbillium_hrm/keymap.c
+++ keyboards/lily58/keymaps/williumbillium_hrm/keymap.c
@@ -22,17 +22,23 @@
  * Homerow mods
  */
 
+/*
+ * 2023/11/21
+ * Convert from Mac to Windows
+ */
+
 #include QMK_KEYBOARD_H
 
 enum layer_number {
   _QWERTY = 0,
   _LOWER,
   _RAISE,
-  _ADJUST,
+  _FUNC,
 };
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define FUNC MO(_FUNC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -47,37 +53,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \BackSp\  |Space |Delete|      |
- *                   |      |      |RAISE |/       /         \      \ |LOWER |      |      |
+ *                   |      | FUNC |RAISE |/       /         \      \ |LOWER |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 
  [_QWERTY] = LAYOUT(
-  XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,                             XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,        XXXXXXX,
-  XXXXXXX, KC_Q,         KC_W,        KC_E,        KC_R,        KC_T,                                KC_Y,              KC_U,        KC_I,        KC_O,        KC_P,           XXXXXXX,
-  XXXXXXX, CTL_T(KC_A),  ALT_T(KC_S), GUI_T(KC_D), SFT_T(KC_F), KC_G,                                KC_H,              SFT_T(KC_J), GUI_T(KC_K), ALT_T(KC_L), CTL_T(KC_QUOT), XXXXXXX,
-  XXXXXXX, KC_Z,         KC_X,        KC_C,        KC_V,        KC_B,              XXXXXXX, XXXXXXX, KC_N,              KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        XXXXXXX,
-                                      XXXXXXX,     KC_ESC,      LT(_RAISE,KC_TAB), KC_ENT,  KC_BSPC, LT(_LOWER,KC_SPC), KC_DEL,      XXXXXXX
+  XXXXXXX, XXXXXXX,      XXXXXXX,     XXXXXXX,     XXXXXXX,          XXXXXXX,                             XXXXXXX,           XXXXXXX,     XXXXXXX,     XXXXXXX,     XXXXXXX,        XXXXXXX,
+  XXXXXXX, KC_Q,         KC_W,        KC_E,        KC_R,             KC_T,                                KC_Y,              KC_U,        KC_I,        KC_O,        KC_P,           XXXXXXX,
+  XXXXXXX, GUI_T(KC_A),  ALT_T(KC_S), CTL_T(KC_D), SFT_T(KC_F),      KC_G,                                KC_H,              SFT_T(KC_J), CTL_T(KC_K), ALT_T(KC_L), GUI_T(KC_QUOT), XXXXXXX,
+  XXXXXXX, KC_Z,         KC_X,        KC_C,        KC_V,             KC_B,              XXXXXXX, XXXXXXX, KC_N,              KC_M,        KC_COMM,     KC_DOT,      KC_SLSH,        XXXXXXX,
+                                      XXXXXXX,     LT(_FUNC,KC_ESC), LT(_RAISE,KC_TAB), KC_ENT,  KC_BSPC, LT(_LOWER,KC_SPC), KC_DEL,      XXXXXXX
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |      |      | Back | Fwd  |VolUp |                    |CMD+Lt|CMD+Dn|CMD+Up|CMD+Rt|Scrnst|      |
+ * |      |      |      | Back | Fwd  |VolUp |                    | Home |CTLEnd|CTL+Hm| End  |Scrnst|      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |VolDn |-------.    ,-------| Left | Down |  Up  |Right |      |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |      |      |ShwDsk|PrvDsk|NxtDsk| Mute |-------|    |-------|ALT+Lt| PgDn | PgUp |ALT+Rt|MaxWin|      |
+ * |      |      |      |PrvDsk|NxtDsk| Mute |-------|    |-------|CTL+Lt| PgDn | PgUp |CTL+Rt|MaxWin|      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \BackSp\  |Space |Delete|      |
- *                   |      | LGUI |RAISE |/       /         \      \ |LOWER |      |      |
+ *                   |      | FUNC |RAISE |/       /         \      \ |LOWER |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 [_LOWER] = LAYOUT(
-  _______, _______, _______, _______,    _______,     _______,                   _______,    _______,        _______,      _______,        _______,    _______,
-  _______, _______, _______, KC_BTN4,    KC_BTN5,     KC_VOLU,                   G(KC_LEFT), G(KC_DOWN),     G(KC_UP),     G(KC_RGHT),     G(S(KC_4)), _______,
-  _______, KC_LCTL, KC_LALT, KC_LGUI,    KC_LSFT,     KC_VOLD,                   KC_LEFT,    SFT_T(KC_DOWN), GUI_T(KC_UP), ALT_T(KC_RGHT), KC_LCTL,    _______,
-  _______, _______, KC_F11,  C(KC_LEFT), C(KC_RIGHT), KC_MUTE, _______, _______, A(KC_LEFT), KC_PGDN,        KC_PGUP,      A(KC_RGHT),     C(G(KC_Z)), _______,
-                             _______,    _______,     _______, _______, _______,  _______,   _______,        _______
+  _______, _______, _______, _______,       _______,        _______,                   _______,    _______,        _______,      _______,        _______,    _______,
+  _______, _______, _______, KC_BTN4,       KC_BTN5,        KC_VOLU,                   KC_HOME,    C(KC_END),      C(KC_HOME),   KC_END,         G(S(KC_4)), _______,
+  _______, KC_LGUI, KC_LALT, KC_LCTL,       KC_LSFT,        KC_VOLD,                   KC_LEFT,    SFT_T(KC_DOWN), CTL_T(KC_UP), ALT_T(KC_RGHT), KC_LGUI,    _______,
+  _______, _______, _______, G(C(KC_LEFT)), G(C(KC_RIGHT)), KC_MUTE, _______, _______, C(KC_LEFT), KC_PGDN,        KC_PGUP,      C(KC_RGHT),     C(G(KC_Z)), _______,
+                             _______,       _______,        _______, _______, _______, _______,    _______,        _______
 ),
 /* RAISE
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -90,37 +96,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |  ~   |  `   |  _   |  [   |  ]   |-------|    |-------|  0   |  1   |  2   |  3   |  \   |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \Backsp\  |Space |Delete|      |
- *                   |      | LGUI | RAISE|/       /         \      \ |LOWER |      |      |
+ *                   |      | FUNC | RAISE|/       /         \      \ |LOWER |      |      |
  *                   `-------------------''-------'           '------''--------------------'
  */
 
 [_RAISE] = LAYOUT(
-  TG(_ADJUST), _______, _______, _______, _______, _______,                     _______, _______,     _______,     _______,     _______,        _______,
+  _______, _______, _______, _______, _______, _______,                     _______, _______,     _______,     _______,     _______,        _______,
   _______,     KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                     KC_PLUS, KC_7,        KC_8,        KC_9,        KC_EQL,         _______,
-  _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                     KC_MINS, SFT_T(KC_4), GUI_T(KC_5), ALT_T(KC_6), CTL_T(KC_SCLN), _______,
+  _______,     KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,                     KC_MINS, SFT_T(KC_4), CTL_T(KC_5), ALT_T(KC_6), GUI_T(KC_SCLN), _______,
   _______,     KC_TILD, KC_GRV,  KC_UNDS, KC_LBRC, KC_RBRC,  _______, _______,  KC_0,    KC_1,        KC_2,        KC_3,        KC_BSLS,        _______,
                                  _______, _______, _______,  _______, _______,  _______, _______,     _______
 ),
 
 /* ADJUST
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * |QWERTY|      |      |      |      |      |                    |      |      |      |      |      |      |
- * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |      |      |      |      |      |                    |      |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |RGBTOG|RGBHUI|RGBSAI|RGBVAI|      |      |-------.    ,-------|      |      |      |      |      |      |
+ * |      |      |      |      |      |      |                    |      |  F7  |  F8  |  F9  | F10  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |RGBTOG|RGBHUI|RGBSAI|RGBVAI|      |      |-------.    ,-------|      |  F4  |  F5  |  F6  | F11  |      |
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
- * |RGBMOD|RGBHUD|RGBSAD|RGBVAD|      |      |-------|    |-------|      |      |      |      |      |      |
+ * |RGBMOD|RGBHUD|RGBSAD|RGBVAD|      |      |-------|    |-------|      |  F1  |  F2  |  F3  | F12  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   |      | Esc  | Tab  | /Enter  /       \BackSp\  |Space |Delete|      |
- *                   |      | LGUI | RAISE|/       /         \      \ |LOWER |      |      |
+ *                   |      | FUNC | RAISE|/       /         \      \ |LOWER |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
-  [_ADJUST] = LAYOUT(
-  TG(_ADJUST), XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RGB_TOG,     RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  RGB_MOD,     RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  [_FUNC] = LAYOUT(
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX,
+  RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                   XXXXXXX, KC_F4,   KC_F5,   KC_F6,   KC_F11,  XXXXXXX,
+  RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F12,  XXXXXXX,
                                  _______, _______, _______, _______, _______,  _______, _______, _______
   )
 };
